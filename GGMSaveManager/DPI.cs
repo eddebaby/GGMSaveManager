e9030738233c1ffd2c0cbb81281ae5c4e8a8648a
eddebaby <ed_j_kearney@yahoo.co.uk>
2022-03-18 16:36:44 +0000

    DPI fixed for Win7 and Win10

---
+++ GGMSaveManager/DPI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+namespace GGMSaveManager
+{
+    class DPI
+    {
+        // from https://stackoverflow.com/questions/5977445/how-to-get-windows-display-settings/5978597#5978597
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int GetDeviceCaps(IntPtr hDC, int nIndex);
+
+        public enum DeviceCap
+        {
+            VERTRES = 10,
+            DESKTOPVERTRES = 117
+        }
+
+        private static double GetWindowsScreenScalingFactor(bool percentage = true)
+        {
+            //Create Graphics object from the current windows handle
+            Graphics GraphicsObject = Graphics.FromHwnd(IntPtr.Zero);
+            //Get Handle to the device context associated with this Graphics object
+            IntPtr DeviceContextHandle = GraphicsObject.GetHdc();
+            //Call GetDeviceCaps with the Handle to retrieve the Screen Height
+            int LogicalScreenHeight = GetDeviceCaps(DeviceContextHandle, (int)DeviceCap.VERTRES);
+            int PhysicalScreenHeight = GetDeviceCaps(DeviceContextHandle, (int)DeviceCap.DESKTOPVERTRES);
+            //DisplayInformation ed111 = Windows.Graphics.Display.DisplayInformation;
+            //float ed = GraphicsObject.DpiY;
+            //Divide the Screen Heights to get the scaling factor and round it to two decimals
+            double ScreenScalingFactor = Math.Round((double)PhysicalScreenHeight / (double)LogicalScreenHeight, 2);
+            //If requested as percentage - convert it
+            if (percentage)
+            {
+                ScreenScalingFactor *= 100.0;
+            }
+            //Release the Handle and Dispose of the GraphicsObject object
+            GraphicsObject.ReleaseHdc(DeviceContextHandle);
+            GraphicsObject.Dispose();
+            //Return the Scaling Factor
+            return ScreenScalingFactor;
+        }
+
+        /// <summary>
+        /// Mulitiplies width and height by the "DPI" scaling value - i.e. if Windows is set to 125% scaling, this will return 1.25 * input.
+        /// </summary>
+        public static int MultiplyByDPIRatio(int input)
+        {
+            int newInt = 0; // (int)(input * DPI.GetWindowsScreenScalingFactor(false));
+            double scalingFactor = DPI.GetWindowsScreenScalingFactor(false);
+            newInt = (int)(input * scalingFactor);
+
+            return newInt;
+        }
+    }
+}
